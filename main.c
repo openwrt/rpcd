e5c9b0411d7ebc03a9edbdd3a7809f0e0bdb38dd
Jo-Philipp Wich <jow@openwrt.org>
2013-05-30 20:17:16 +0200

    Add system namespace which offers various system information like software release, memory and swap info, uptime and load

--- main.c
+++ main.c
@@ -27,6 +27,7 @@
 #include "file.h"
 #include "uci.h"
 #include "iwinfo.h"
+#include "system.h"
 
 static struct ubus_context *ctx;
 
@@ -64,6 +65,7 @@ int main(int argc, char **argv)
 	rpc_file_api_init(ctx);
 	rpc_uci_api_init(ctx);
 	rpc_iwinfo_api_init(ctx);
+	rpc_system_api_init(ctx);
 
 	uloop_run();
 	ubus_free(ctx);
