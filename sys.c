openwrt/rpcd

commit 27c24c7c186ad6522995872a9fe380dd1545af9a

rpcd: sys: actually move timespec declaration
Move the timespec declaration to the function header, as mentioned in
commit "rpcd: Switch to nanosleep".

Fixes: 604db20 ("rpcd: Switch to nanosleep")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

sys.c
@@ -88,6 +88,7 @@ rpc_cgi_password_set(struct ubus_context *ctx, struct ubus_object *obj,
 	ssize_t n;
 	int ret;
 	const char *const passwd = "/bin/passwd";
+	const struct timespec req = {0, 100 * 1000 * 1000};
 
 	blobmsg_parse(rpc_password_policy, __RPC_P_MAX, tb,
 	              blob_data(msg), blob_len(msg));
@@ -145,7 +146,6 @@ rpc_cgi_password_set(struct ubus_context *ctx, struct ubus_object *obj,
 		if (n < 0)
 			return rpc_errno_status();
 
-		const struct timespec req = {0, 100 * 1000 * 1000};
 		nanosleep(&req, NULL);
 
 		n = write(fds[1], blobmsg_data(tb[RPC_P_PASSWORD]),

commit 7fec63611d1698795dcc10ffe1f1526a235189f8

sys: fix symbol redeclaration
Fixes: 27c24c7 ("rpcd: sys: actually move timespec declaration")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

sys.c
@@ -88,7 +88,7 @@ rpc_cgi_password_set(struct ubus_context *ctx, struct ubus_object *obj,
 	ssize_t n;
 	int ret;
 	const char *const passwd = "/bin/passwd";
-	const struct timespec req = {0, 100 * 1000 * 1000};
+	const struct timespec ts = {0, 100 * 1000 * 1000};
 
 	blobmsg_parse(rpc_password_policy, __RPC_P_MAX, tb,
 	              blob_data(msg), blob_len(msg));
@@ -146,7 +146,7 @@ rpc_cgi_password_set(struct ubus_context *ctx, struct ubus_object *obj,
 		if (n < 0)
 			return rpc_errno_status();
 
-		nanosleep(&req, NULL);
+		nanosleep(&ts, NULL);
 
 		n = write(fds[1], blobmsg_data(tb[RPC_P_PASSWORD]),
 		              blobmsg_data_len(tb[RPC_P_PASSWORD]) - 1);