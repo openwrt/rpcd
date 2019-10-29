openwrt/rpcd

commit 37aa9196b603769ffbff4d0c58f76259a3791384

plugin: fix leaking invoked method name for exec plugins
The invoked method name was separately duplicated from the call_context
structure. The structure itself is eventually freed by rpc_exec_reply()
but the method string it points to is lost after that.

Use calloc_a() instead to allocate the string copy buffer together with
the context structure, to ensure that all involved memory is freed.

Signed-off-by: Jo-Philipp Wich <jo@mein.io>

plugin.c
@@ -135,14 +135,14 @@ rpc_plugin_call(struct ubus_context *ctx, struct ubus_object *obj,
 {
 	int rv = UBUS_STATUS_UNKNOWN_ERROR;
 	struct call_context *c;
-	char *plugin;
+	char *plugin, *mptr;
 
-	c = calloc(1, sizeof(*c));
+	c = calloc_a(sizeof(*c), &mptr, strlen(method) + 1);
 
 	if (!c)
 		goto fail;
 
-	c->method = strdup(method);
+	c->method = strcpy(mptr, method);
 	c->input = blobmsg_format_json(msg, true);
 	c->tok = json_tokener_new();
 

commit ca3e2d5e70f188cc73a9bd1dadd3dcf2603bdba7

plugin: do not free method name separately
A previous commit changed the allocation method for the call context to
include the method name string memory directly, so we must not treat the
method member separately anymore.

Fixes: 37aa919 ("plugin: fix leaking invoked method name for exec plugins")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

plugin.c
@@ -146,7 +146,7 @@ rpc_plugin_call(struct ubus_context *ctx, struct ubus_object *obj,
 	c->input = blobmsg_format_json(msg, true);
 	c->tok = json_tokener_new();
 
-	if (!c->method || !c->input || !c->tok)
+	if (!c->input || !c->tok)
 		goto fail;
 
 	plugin = c->path + sprintf(c->path, "%s/", RPC_PLUGIN_DIRECTORY);
@@ -171,9 +171,6 @@ rpc_plugin_call(struct ubus_context *ctx, struct ubus_object *obj,
 fail:
 	if (c)
 	{
-		if (c->method)
-			free(c->method);
-
 		if (c->input)
 			free(c->input);
 